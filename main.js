089887be658338519d49c7003c28f07f6b5c727d
Pinja Alanne <pinja.alanne@gmail.com>
2023-09-27 09:40:38 +0300

    updated modal

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const playButton = document.querySelector('#play')
 const stopButton = document.querySelector('#stop')
 const clowns = document.querySelectorAll('.clowncircle')
 const showScore = document.querySelector('.score')
+const modal = document.querySelector('.modal')
+const modalHeader = document.querySelector('#modalheader')
+const modalText = document.querySelector('#modaltext')
+const closingButton = document.querySelector('#xButton')
 
 let score = 0;
 let timer;
@@ -11,6 +15,22 @@ let rounds = 0;
 
 const getRandomI = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const modalBox = () => {
+    if(score >= 15) {
+        modalHeader.textContent = `HMM...`
+        modalText.textContent = `That was ok, but try again or you're in trouble`
+        showScore.textContent = score;
+    } else if(score >= 500) {
+        modalHeader.textContent = `You killer`
+        modalText.textContent = `Good job! You killed all the clowns! For now...`
+        showScore.textContent = score
+    } else {
+        modalHeader.textContent = `GAME OVER`
+        modalText.textContent = `What was that? You should watch your back!`
+        showScore.textContent = score
+    }
 }
 
 const clickClown = (i) => {
@@ -61,6 +81,7 @@ if(rounds >= 3) {
 }
 
 const stopGame = () => {
+    modal.classList.toggle('visible')
     clearTimeout(timer)
     resetGame()
 }
@@ -70,4 +91,5 @@ const resetGame = () => {
 }
 
 playButton.addEventListener('click', playGame)
-stopButton.addEventListener('click', stopGame)
+stopButton.addEventListener('click', stopGame, modalBox)
+closingButton.addEventListener('click', modalBox)
